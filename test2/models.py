b444aef13b1d91042368fa2db7c98f4d2edb26a4
Aika1988 <dzhumagulovaaika88@icloud.com>
2023-02-02 15:04:44 +0600

    changed

--- test2/models.py
+++ test2/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from test1.models import Post
 
 class Comment(models.Model):
     comment = models.TextField()
+    post_comment= models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
 
 
     def __str__(self):
